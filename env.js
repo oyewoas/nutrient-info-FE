c7c75e5dbe5604ba665347c1c56e132c582677a8
Ayooluwa Oyewo <ayooluwaoyewo@gmail.com>
2019-09-17 13:11:08 +0100

    optimizing folder structure and architecture

---
+++ env.js
@@ -0,0 +1,3 @@
+export default {
+    api: process.env.BASE_API_URL,
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'jquery/dist/jquery.min';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.css';
+import './assets/styles/normalize.css';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App.js";
-ReactDOM.render(<App />, document.getElementById("root"));
+const routing = (
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={App} />
+        </Switch>
+      </div>
+    </Router>
+  )
+ReactDOM.render(routing, document.getElementById('root'));
